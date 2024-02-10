adf2b299aea87035a0a63517aa795002898fb398
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-02-11 01:38:36 +0530

    Create LCSOfThreeStrings.java

---
+++ LCSOfThreeStrings.java
@@ -0,0 +1,27 @@
+//User function Template for Java
+
+class Solution 
+{ 
+    int LCSof3(String A, String B, String C, int n1, int n2, int n3) 
+    { 
+        // code here
+        int[][][] dp = new int[n1 + 1][n2 + 1][n3 + 1];
+
+        // Build the dp array using bottom-up approach
+        for (int i = 0; i <= n1; i++) {
+            for (int j = 0; j <= n2; j++) {
+                for (int k = 0; k <= n3; k++) {
+                    if (i == 0 || j == 0 || k == 0)
+                        dp[i][j][k] = 0;
+                    else if (A.charAt(i - 1) == B.charAt(j - 1) && A.charAt(i - 1) == C.charAt(k - 1))
+                        dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
+                    else
+                        dp[i][j][k] = Math.max(Math.max(dp[i - 1][j][k], dp[i][j - 1][k]), dp[i][j][k - 1]);
+                }
+            }
+        }
+
+        // Return the length of the longest common subsequence
+        return dp[n1][n2][n3];
+    }
+}
