17083a71dfaff201c2b0c75f47a0333feb6cd745
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-02-15 15:29:13 +0530

    Create CountAllPossiblePath.java

---
+++ CountAllPossiblePath.java
@@ -0,0 +1,16 @@
+//User function Template for Java
+
+class Solution
+{
+    public int isPossible(int[][] paths)
+    {
+        for(int i=0;i<paths.length;i++){
+            int c=0;
+            for(int j=0;j<paths.length;j++){
+                if(paths[i][j]==1)c++;
+            }
+            if(c%2==1)return 0;
+        }
+        return 1;
+    }
+}
