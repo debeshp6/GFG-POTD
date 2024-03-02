aa2bd06e2f481a3e351e44cc713d10a46c5aaf57
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-03-02 20:17:52 +0530

    Create FirstElementToOccurKTimes.java

---
+++ FirstElementToOccurKTimes.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public int firstElementKTime(int n, int k, int[] a) { 
+        Map<Integer,Integer>mp=new HashMap<>();
+        for(int i=0;i<n;i++){
+            mp.put(a[i],mp.getOrDefault(a[i],0)+1);
+            if(mp.containsKey(a[i])&&mp.get(a[i])==k){
+                return a[i];
+            }
+        }
+     return -1;
+    } 
+}
