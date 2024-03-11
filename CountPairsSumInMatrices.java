185d851fba084af2968007b45ec9b9d07b40f306
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-03-11 20:26:04 +0530

    Create CountPairsSumInMatrices.java

---
+++ CountPairsSumInMatrices.java
@@ -0,0 +1,21 @@
+//User function Template for Java
+
+class Solution {
+    int countPairs(int mat1[][], int mat2[][], int n, int x) {
+        int count=0;
+        HashSet<Integer> hs=new HashSet<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                hs.add(mat1[i][j]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(hs.contains(x-mat2[i][j])){
+                    count++;
+                }
+            }
+        } 
+      return count;
+    }
+}
