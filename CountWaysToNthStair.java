231194c08b5abbcea6025c70f2e9e8a57e857a6d
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-04-06 23:15:37 +0530

    Create CountWaysToNthStair.java

---
+++ CountWaysToNthStair.java
@@ -0,0 +1,7 @@
+class Solution {
+    // Function to count number of ways to reach the nth stair
+    // when order does not matter.
+    Long countWays(int n) {
+        return (long) n/2+1L;
+    }
+}
