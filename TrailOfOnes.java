fd6e5337e6f6ccccf14b935a589227127308251e
Debesh Paul <139678494+debeshp6@users.noreply.github.com>
2024-06-03 23:04:04 +0530

    Create TrailOfOnes.java

---
+++ TrailOfOnes.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+    static int numberOfConsecutiveOnes(int n) {
+        if(n == 2) return 1;
+        int a = 0, b = 1;
+        int ans = 1;
+        int mod = 1_000_000_007;
+        for(int i=3; i<=n; i++){
+            int c = (a+b)%mod;
+            a = b;
+            b = c;
+            ans = ((ans*2)%mod + c)%mod;
+        }
+        return ans;
+    }
+}
